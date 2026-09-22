057678738ec1aeea4f9646d257463650dceae1dc
Your Name <you@example.com>
2026-09-22 07:52:46 +0530

    first commit

---
+++ api-management-service/src/test/java/in/subhadeep/gatekeeper/management/ApiManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.subhadeep.gatekeeper.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
